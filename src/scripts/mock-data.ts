789a542f674a4a7c416521f28a7b7cc5bee758b3
Test <ndthanh2111@gmail.com>
2016-11-28 22:43:18 +0700

    Đã hoàn thành

--- src/scripts/mock-data.ts
+++ src/scripts/mock-data.ts
@@ -15,11 +15,30 @@ pg.connect(connectString, (err,client,done) => {
 
     client.query('BEGIN')
         .then(() => {
-            return client.query('INSERT INTO "ioh_TienIch" VALUES ($1,$2,$3,$4)');
+            return client.query('INSERT INTO "ioh_TienIch" VALUES (1,"test1","test1","http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png")');
         })
         .then(() => {
-            return client.query('INSERT INTO "ioh_LoaiTinDang" VALUES ($1,$2,$3,$4)');
+            return client.query('INSERT INTO "ioh_TienIch" VALUES (2,"test2","test2","http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png")');
         })
+         .then(() => {
+            return client.query('INSERT INTO "ioh_TienIch" VALUES (3,"test3","test3","http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png")');
+        })
+         .then(() => {
+            return client.query('INSERT INTO "ioh_TienIch" VALUES (4,"test4","test4","http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png")');
+        })
+
+        .then(() => {
+        return client.query('INSERT INTO "ioh_LoaiTinDang" VALUES (1,"tindang1","tindang1","http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png")');
+         })
+           .then(() => {
+        return client.query('INSERT INTO "ioh_LoaiTinDang" VALUES (2,"tindang2","tindang2","http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png")');
+         })
+           .then(() => {
+        return client.query('INSERT INTO "ioh_LoaiTinDang" VALUES (3,"tindang3","tindang3","http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png")');
+         })
+           .then(() => {
+        return client.query('INSERT INTO "ioh_LoaiTinDang" VALUES (4,"tindang4","tindang4","http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png")');
+         })
         .catch(error => {
             console.error('Error in create mock data',error);
             client.query('ROLLBACK');
